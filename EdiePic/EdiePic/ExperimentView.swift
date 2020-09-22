178ebab07b01b7e0524c0b0a60c51efcf76c95a7
Abhisek <abhisekbunty94@gmail.com>
2020-09-22 17:19:51 +0530

    Used common model for filters and selected image

--- EdiePic/EdiePic/ExperimentView.swift
+++ EdiePic/EdiePic/ExperimentView.swift
@@ -9,8 +9,34 @@
 import SwiftUI
 
 struct ExperimentView: View {
+    @State var bodyText: String = "Hello swift"
+    private var mutableBodyText: String = "Hello swift" {
+        didSet {
+            bodyText = mutableBodyText
+        }
+    }
+    
+    init() {
+        triggerTextUpdateViaDidSet()
+    }
+    
     var body: some View {
-        Text("Hello swift")
+        Text(bodyText)
+                .onAppear(perform: triggerTextUpdate)
+    }
+    
+    private mutating func triggerTextUpdateViaDidSet() {
+        var result =  self
+        DispatchQueue.main.asyncAfter(deadline: .now()+3) {
+            result.mutableBodyText = "Hello I am future swift"
+        }
+        self = result
+    }
+    
+    private func triggerTextUpdate() {
+        DispatchQueue.main.asyncAfter(deadline: .now()+3) {
+            bodyText = "Hello I am future swift"
+        }
     }
 }
 
--- EdiePic/EdiePic/ImagePicker.swift
+++ EdiePic/EdiePic/ImagePicker.swift
@@ -13,8 +13,7 @@ import Combine
 
 struct ImagePicker: UIViewControllerRepresentable {
     var sourceType: UIImagePickerController.SourceType = .photoLibrary
-    @Binding var selectedImage: Image?
-    @Binding var filteredImage: [Image]?
+    @Binding var filterConfig: FilterConfig
     @Environment(\.presentationMode) var presentationMode
 
     func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
@@ -43,65 +42,10 @@ struct ImagePicker: UIViewControllerRepresentable {
         
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-                parent.selectedImage = Image(uiImage: image)
-                prepareFilters(image: image)
-                parent.filteredImage = filteredImage
+                parent.filterConfig.userSelectedImage(image: image)
             }
             
             parent.presentationMode.wrappedValue.dismiss()
         }
-    }
-}
-
-// MARK: PRepare filters
-extension ImagePicker.Coordinator {
-    private func prepareFilters(image: UIImage) {
-        if let image = getSepiaImageForSelectedImage(inputImage: image) {
-            store(filteredImage: image)
-        }
-        
-        if let image = getVignetteImageForSelectedImage(inputImage: image) {
-            store(filteredImage: image)
-        }
-    }
-    
-    private func store(filteredImage: Image) {
-        if let filteredImages = self.filteredImage {
-            self.filteredImage = filteredImages + [filteredImage]
-        } else {
-            self.filteredImage = [filteredImage]
-        }
-    }
-    
-    private func getSepiaImageForSelectedImage(inputImage: UIImage) -> Image? {
-        let beginImage = CIImage(image: inputImage)
-        
-        let context = CIContext()
-        let currentFilter = CIFilter.sepiaTone()
-        currentFilter.inputImage = beginImage
-        currentFilter.intensity = 1
-        
-        guard let outputImage = currentFilter.outputImage,
-        let cgimg = context.createCGImage(outputImage, from: outputImage.extent) else {
-            return nil
-        }
-        
-        return Image(uiImage: UIImage(cgImage: cgimg))
-    }
-    
-    private func getVignetteImageForSelectedImage(inputImage: UIImage) -> Image? {
-        let beginImage = CIImage(image: inputImage)
-        
-        let context = CIContext()
-        let currentFilter = CIFilter.vignette()
-        currentFilter.inputImage = beginImage
-        currentFilter.intensity = 5
-        
-        guard let outputImage = currentFilter.outputImage,
-        let cgimg = context.createCGImage(outputImage, from: outputImage.extent) else {
-            return nil
-        }
-        
-        return Image(uiImage: UIImage(cgImage: cgimg))
     }
 }
